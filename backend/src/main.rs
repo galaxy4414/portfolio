eb5476b780f46fda34ab45478c71e626b594b1d8
galaxy4414 <galaxy4414@tars.kr>
2022-11-09 01:01:17 +0900

    tokio-postgres 를 이용한 db 쿼리 테스트

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,8 +1,34 @@
 #[macro_use] extern crate rocket;
+extern crate tokio_postgres;
+extern crate tokio;
+
+use tokio_postgres::{NoTls};
+
+async fn test_db() -> Result<(), tokio_postgres::Error> {
+    let (client, connection) = tokio_postgres::connect("host=localhost user=postgres password=hello1234$#@! dbname=portfolio_test", NoTls).await?;
+
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+
+    let ret =  client.query("SELECT id, name FROM tb_user;", &[]).await?;
+
+    for row in ret {
+        let id: i32 = row.get(0);
+        let name: &str = row.get(1);
+    
+        println!("found user: {} {}", id, name);
+    }
+
+    return Ok(())
+}
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+async fn index() -> &'static str {
+    test_db().await;
+    return "Hello, world!"
 }
 
 #[launch]
